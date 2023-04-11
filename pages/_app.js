71e228212d1882d504b4d4c281040df40c2d392c
Vivek <vivekthakur6259@gmail.com>
2023-04-11 18:36:57 +0530

    :construction: feed, upload btn, videos

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import './signup/signup.css'
 import './login/login.css'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import '../components/Feed.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
