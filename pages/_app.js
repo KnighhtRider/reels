d23161ffff12c9eb2f2f8cf663f39a80f24844e1
Vivek <vivekthakur6259@gmail.com>
2023-04-11 16:45:37 +0530

    :lipstick: login UI completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '@/styles/globals.css'
 import './signup/signup.css'
+import './login/login.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/signup/index.js
+++ pages/signup/index.js
@@ -2,30 +2,52 @@ import React from 'react'
 import Image from 'next/image'
 import logo from '../../assets/Instagram.jpeg'
 import TextField from "@mui/material/TextField";
-import Box from '@mui/material/Box';
+// import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 function index() {
   return (
-    <div className='signup-cont'>
+    <div className='signup-container'>
       <div className='signup-card'>
         <Image src={logo} />
-        <TextField id="outlined-basic" label="Email" variant="outlined"
+        <TextField 
+          id="outlined-basic" 
+          label="Email" 
+          variant="outlined"
           margin='dense'
           fullWidth />
-        <TextField id="outlined-basic" label="Password" variant="outlined"
+        <TextField 
+          id="outlined-basic" 
+          label="Password" 
+          variant="outlined"
           margin='dense'
           fullWidth />
-        <TextField id="outlined-basic" label="Full Name" variant="outlined" 
+        <TextField 
+          id="outlined-basic" 
+          label="Full Name" 
+          variant="outlined" 
           margin='dense'  
           fullWidth />
-        <Button variant="outlined" 
+        <Button 
+          variant="outlined" 
           color="secondary" 
           fullWidth>
           <CloudUploadIcon />
           Upload Profile Image
+          <input hidden accept="image/*" type="file" />
         </Button>
+        <Button style={{ marginTop: "1rem" }} variant="contained" fullWidth>
+            Sign Up
+        </Button>
+        <div className="tnc">
+          By signing up, you agree to our Terms, Conditions and Cookies
+          policy.
+        </div>
+      </div>
+      <div className="bottom-card">
+          Already Have an account ? {" "}
+          <span style={{ color: "blueviolet" }}>Login</span>
       </div>
     </div>
   )
